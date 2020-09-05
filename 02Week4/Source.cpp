022906573ceb93077429b4cb2edff50f7e9d0f41
Patcharaphon Chokkun <davilsek@hotmail.com>
2020-09-05 13:34:32 +0700

    Add project files.

---
+++ 02Week4/Source.cpp
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+    char row[8][9];
+    int enemyScore = 0, myScore = 0, total;
+    for (int i = 0; i < 8; i++)
+    {
+        scanf("%s", row[i]);
+    }
+    for (int a = 0; a < 8; a++)
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            switch (row[a][i])
+            {
+            case 'P': myScore += 1;
+                break;
+            case 'N': myScore += 3;
+                break;
+            case 'B': myScore += 3;
+                break;
+            case 'R': myScore += 5;
+                break;
+            case 'Q': myScore += 9;
+                break;
+            case 'p': enemyScore += 1;
+                break;
+            case 'n': enemyScore += 3;
+                break;
+            case 'b': enemyScore += 3;
+                break;
+            case 'r': enemyScore += 5;
+                break;
+            case 'q': enemyScore += 9;
+                break;
+            default:
+                break;
+            }
+        }
+    }
+    printf("%d", myScore - enemyScore);
+}
---
+++ 03Week4/Source.cpp
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+    char text[100];
+    int len;
+    scanf("%s", text);
+    len = strlen(text);
+    for (int i = 0; i < len; i++)
+    {
+        for (int j = len - i; j >= 0; j--)
+        {
+            printf("  ");
+        }
+        for (int j = 0; j <= i; j++)
+        {
+            printf("%c ", text[i - j]);
+        }
+        for (int j = 0; j < i; j++)
+        {
+            printf("%c ", text[j + 1]);
+        }
+        printf("\n");
+    }
+}
