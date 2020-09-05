87021ddeb746d7187148e93a1f305f34aeaabb58
Phacharaphol Chokkhun <66435789+ZionSS@users.noreply.github.com>
2020-09-05 13:45:50 +0700

    Update Source.cpp

--- 02Week4/Source.cpp
+++ 02Week4/Source.cpp
@@ -1,44 +1,51 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<string.h>
 
-int main() {
-    char row[8][9];
-    int enemyScore = 0, myScore = 0, total;
-    for (int i = 0; i < 8; i++)
+int main(){
+   char row[8][9];
+   int enemyScore=0,myScore=0,total;
+   for(int i = 0 ; i<8;i++)
+   {
+       scanf("%s",row[i]);
+   }
+    for(int a = 0;a<8;a++)
     {
-        scanf("%s", row[i]);
-    }
-    for (int a = 0; a < 8; a++)
-    {
-        for (int i = 0; i < 9; i++)
+            for(int i = 0;i<9;i++)
         {
             switch (row[a][i])
             {
-            case 'P': myScore += 1;
-                break;
-            case 'N': myScore += 3;
-                break;
-            case 'B': myScore += 3;
-                break;
-            case 'R': myScore += 5;
-                break;
-            case 'Q': myScore += 9;
-                break;
-            case 'p': enemyScore += 1;
-                break;
-            case 'n': enemyScore += 3;
-                break;
-            case 'b': enemyScore += 3;
-                break;
-            case 'r': enemyScore += 5;
-                break;
-            case 'q': enemyScore += 9;
-                break;
-            default:
-                break;
+                case 'P' : myScore+=1;
+                    break;
+                case 'N' : myScore+=3;
+                    break;
+                case 'B' : myScore+=3;
+                    break;
+                case 'R' : myScore+=5;
+                    break;
+                case 'Q' : myScore+=9;
+                    break;
+                case 'p' : enemyScore+=1;
+                    break;
+                case 'n' : enemyScore+=3;
+                    break;
+                case 'b' : enemyScore+=3;
+                    break;
+                case 'r' : enemyScore+=5;
+                    break;
+                case 'q' : enemyScore+=9;
+                    break;
+                default:
+                    break;
             }
         }
     }
-    printf("%d", myScore - enemyScore);
+    if(myScore-enemyScore!=0)
+    {
+        printf("%d",myScore-enemyScore);
+    }
+    else
+    {
+        printf("equal");
+    }
+    
 }
